a0f6ff93772de41a7019744f89c6f4bd5583eda5
tvitcom <tvitcom@yandex.ru>
2018-12-17 00:19:56 +0200

    fix debug mode select

--- index.php
+++ index.php
@@ -3,10 +3,22 @@
 $yii = dirname(__FILE__) . '/vendor/yiisoft/yii/framework/yii.php';
 $config = dirname(__FILE__) . '/protect/config/main.php';
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+// Select application mode:
+if (
+    $_SERVER['REMOTE_ADDR'] === '127.0.0.1' 
+    || $_SERVER['REMOTE_ADDR'] === '192.168.10.64'
+    || $_SERVER['REMOTE_ADDR'] === '192.168.10.123'
+    ) {
+    $mode= true;
+    $trace_level=4;
+} else {
+    $mode=false;
+    $trace_level=0;
+}
+
+defined('YII_DEBUG') or define('YII_DEBUG', $mode);
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 4);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', $trace_level);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
